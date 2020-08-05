aff3c4e26130ab58a458f5cde99d27e6b24643ef
somtom <som_tom@web.de>
2020-08-05 20:41:30 +0200

    adjusted function to use token

--- functions/create-alert-post.js
+++ functions/create-alert-post.js
@@ -5,36 +5,37 @@ const Storyblok = new StoryblokClient({
 })
 
 exports.handler = function (event, context, callback) {
-  console.log(event)
-  console.log(context)
   const alert = JSON.parse(event.body)
-  console.log('Alert: ', alert)
-  console.log('TOKEN: ', ALERT_FUNCTION_TOKEN)
+  let statusCode = 401
 
-  // Create alert story
-  Storyblok.post('spaces/90003/stories/', {
-    story: {
-      name: alert.id,
-      slug: alert.id,
-      parent_id: 16915628,
-      content: {
-        component: 'AlertPost',
-        title: alert.title || alert.message,
-        time: new Date(alert.ts_create * 1000),
-        address: alert.address,
-        message: alert.text,
+  if (ALERT_FUNCTION_TOKEN === event.queryStringParameters.token) {
+    // Create alert story
+    Storyblok.post('spaces/90003/stories/', {
+      story: {
+        name: alert.id,
+        slug: alert.id,
+        parent_id: 16915628,
+        content: {
+          component: 'AlertPost',
+          title: alert.title || alert.message,
+          time: new Date(alert.ts_create * 1000),
+          address: alert.address,
+          message: alert.text,
+        },
       },
-    },
-    publish: 1,
-  })
-    .then((response) => {
-      console.log(response)
+      publish: 1,
     })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+    statusCode = 200
+  }
 
   callback(null, {
-    statusCode: 200,
+    statusCode,
   })
 }
