e258990ceec7c89b01b3ef7dee6f6b4aba3d482e
somtom <som_tom@web.de>
2020-08-08 10:12:42 +0200

    added pagination

--- pages/alerts/index.vue
+++ pages/alerts/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h1 class="mb-6">Einsatzübersicht</h1>
+
     <AlertPreview
       v-for="alert in alerts"
       :key="alert.full_slug"
@@ -7,27 +9,45 @@
       :img-src="previewImage(alert)"
       v-bind="alert.content"
     ></AlertPreview>
+
+    <Paginator v-model="paginator" />
   </div>
 </template>
 
 <script>
 import AlertPreview from '@/components/AlertPreview'
+import Paginator from '@/components/Paginator'
 import query from './query.graphql'
 export default {
   components: {
     AlertPreview,
+    Paginator,
   },
 
   data() {
     return {
       alerts: [],
+      paginator: {
+        totalEntries: null,
+        pageSize: 10,
+        currentPage: 1,
+      },
     }
   },
 
   apollo: {
     alerts: {
       query,
+      variables() {
+        return {
+          pageSize: this.paginator.pageSize,
+          currentPage: this.paginator.currentPage,
+          dateStart: null,
+          dateEnd: null,
+        }
+      },
       update(data) {
+        this.paginator.totalEntries = data.AlertpostItems.total
         return data.AlertpostItems.items
       },
     },
@@ -35,7 +55,6 @@ export default {
 
   methods: {
     previewImage(alert) {
-      console.log(alert)
       return alert.content.previewImage
         ? alert.content.previewImage.filename
         : ''
