e58f64070c2637a402ba0a71f18dfb1b4c86e671
somtom <som_tom@web.de>
2020-08-30 17:00:05 +0200

    ajdusted alert function to not publish test alerts

--- functions/create-alert-post.js
+++ functions/create-alert-post.js
@@ -1,40 +1,89 @@
 const StoryblokClient = require('storyblok-js-client')
-const { STORYBLOK_API_KEY, ALERT_FUNCTION_TOKEN } = process.env
+const axios = require('axios')
+
+const {
+  STORYBLOK_API_KEY,
+  ALERT_FUNCTION_TOKEN,
+  DIVERA_ACCESS_KEY,
+  STORYBLOK_SPACE_ID,
+  STORYBLOK_ALERT_FOLDER,
+} = process.env
+
 const Storyblok = new StoryblokClient({
   oauthToken: STORYBLOK_API_KEY,
 })
 
-exports.handler = function (event, context, callback) {
-  const alert = JSON.parse(event.body)
-  console.log('Alert: ', alert)
-  let statusCode = 401
+async function getDiveraAlert(id, callback) {
+  /* 
+  Wait for 5 Seconds to receive second alarm which completes the information
+  This can be removed as soon as the SMS alarm fallback is removed
+  */
+  await new Promise((resolve, reject) => setTimeout(resolve, 5000))
 
-  if (ALERT_FUNCTION_TOKEN === event.queryStringParameters.token) {
-    // Create alert story
-    const time = new Date(alert.ts_create * 1000)
-    const name = `${time.toISOString().substr(0, 10)} - ${alert.title}`
-    Storyblok.post('spaces/90003/stories/', {
-      story: {
-        name,
-        slug: alert.id,
-        parent_id: 16915628,
-        content: {
-          component: 'AlertPost',
-          title: alert.title || alert.message,
-          time,
-          address: alert.address,
-          message: alert.title,
-        },
-      },
-      publish: 0,
-    }).catch((error) => {
-      console.log(error)
+  // Get alarm detail data
+  const url = `https://www.divera247.com/api/v2/alarms/${id}`
+  return axios
+    .get(url, { params: { accesskey: DIVERA_ACCESS_KEY } })
+    .then((res) => {
+      return res.data.data
     })
+    .catch((error) => console.log(error))
+}
 
-    statusCode = 200
+function createStoryFromAlert(alert) {
+  const time = new Date(alert.ts_create * 1000)
+  const name = `${time.toISOString().substr(0, 10)} - ${alert.title}`
+
+  const story = {
+    name,
+    slug: alert.id,
+    parent_id: STORYBLOK_ALERT_FOLDER,
+    content: {
+      component: 'AlertPost',
+      title: alert.title || alert.message,
+      time,
+      address: alert.address,
+      message: alert.title,
+    },
   }
 
-  callback(null, {
-    statusCode,
+  // Only publish if publish criteria fulfilled
+  const regex = RegExp('(^|\\b)(probealarm|test)(\\b|$)')
+  const isTestAlert =
+    regex.test(alert.title.toLocaleLowerCase()) ||
+    regex.test(alert.title.toLocaleLowerCase())
+  const canBePublished = !isTestAlert && !alert.private_mode && alert.priority
+  if (canBePublished) {
+    story.publish = 1
+  }
+
+  Storyblok.post(`spaces/${STORYBLOK_SPACE_ID}/stories/`, {
+    story,
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+exports.handler = function (event, context, callback) {
+  let statusCode = 401
+
+  if (!ALERT_FUNCTION_TOKEN === event.queryStringParameters.token) {
+    callback(null, {
+      statusCode,
+    })
+  }
+
+  // Get alert data from Divera to be able to check if it is from the control center
+  const webhookData = JSON.parse(event.body)
+  getDiveraAlert(webhookData.id, callback).then((alert) => {
+    console.log('Alert: ', alert)
+    // Create alert story
+    createStoryFromAlert(alert)
+
+    statusCode = 200
+
+    callback(null, {
+      statusCode,
+    })
   })
 }
