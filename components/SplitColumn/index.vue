98b0c799d247d1176c92e5e656bdef6b01c12614
somtom <som_tom@web.de>
2020-11-26 19:54:32 +0100

    added new split column component

---
+++ components/SplitColumn/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-wrap justify-between">
+    <div><RhichText :text="getRichText(textLeft)" /></div>
+    <div><RhichText :text="getRichText(textRight)" /></div>
+  </div>
+</template>
+
+<script>
+import RhichText from '@/components/RichText'
+export default {
+  components: { RhichText },
+
+  props: {
+    textLeft: {
+      type: Array,
+      default: () => [],
+    },
+    textRight: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  methods: {
+    getRichText(textArray) {
+      return textArray.length ? textArray[0].text : []
+    },
+  },
+}
+</script>
--- components/storyblok/storyblokComponents.js
+++ components/storyblok/storyblokComponents.js
@@ -2,10 +2,12 @@ import AlertPost from '@/components/AlertPost'
 import Post from '@/components/Post'
 import RichText from '@/components/RichText'
 import ImageGrid from '@/components/ImageGrid'
+import SplitColumn from '@/components/SplitColumn'
 
 export default {
   AlertPost,
   Post,
   RichText,
   ImageGrid,
+  SplitColumn,
 }
